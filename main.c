53bab3f4ea6f107a9f6c56afbe6513d11fbb5fac
samurai0lava <ouhsseineilyass@gmail.com>
2024-04-19 12:55:11 +0100

    Add

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include "mini_talk.h"
 
-void sig_handler(int sig)
+void signal_handler(int sig)
 {
-    printf("\nallo %d\n", sig);
+    printf("Signal received: %d\n", sig);
 }
+
 int main(int argc, char **argv)
 {
-    signal(SIGINT, SIG_ERR);
-    while(1)
+    struct sigaction sa;
+    sa.sa_handler = &signal_handler;
+    sigaction(SIGTSTP, &sa, NULL);
+
+    while (1)
+    {
+        printf("Hello, world!\n");
         sleep(1);
-    return (0);
+    }
+
 }
 
