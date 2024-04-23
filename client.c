c974d3ad07c3b2e9714e67d3502488657df7b27f
Ilyass Ouhsseine <iouhssei@e2r4p8.1337.ma>
2024-04-23 10:45:50 +0100

    add some functions and understand more the project and set the algorithm to go with

--- client.c
+++ client.c
@@ -1,16 +1,53 @@
 #include "mini_talk.h"
 
+static void send_message(pid_t pid, int message)
+{
+	kill(pid, SIGUSR1 | message);
+}
+
+static int char_to_bin(char *str)
+{
+	int i;
+
+	i = 0;
+	/*
+		convert string to binary to send it with siguser1
+		using bitwise operations
+	*/
+}
+static int ft_isdigit_adv(char *argv)
+{
+	int i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (ft_isdigit(argv[i]) == 1)
+			i++;
+		else
+			return (0);
+	}
+	return(1);
+}
+
+   
 int main(int argc, char **argv)
 {
-	pid_t pid;
-
-	if(argc == 1)
+	pid_t				pid;
+	int					i;
+	int message;
+	// struct sigaction	sa;
+	// sa.sa_handler = &handler;
+	// sigaction(SIGINT, &sa, NULL);
+	if(argc == 2 && ft_isdigit_adv(argv[1]) == 1)
 	{
+		message = char_to_bin(argv[2]);
+		pid = ft_atoi(argv[1]);
 		while(1)
 		{
-			pid = getpid();
-			ft_printf("%d\n", pid);
+			send_message(pid, message);
+			sleep(1); //wait the server to process the message sent
 		}
 	}
-	return(0);
+
 }
--- server.c
+++ server.c
@@ -1,42 +1,27 @@
 #include "mini_talk.h"
 
-static void handler(int sig)
-{
-	write(1, "hello\n", 6);
-}
-
-static void handler1(int sig)
-{
-	write(1, "hello1\n", 6);
-}
-int ft_isdigit_adv(char *argv)
+static bin_to_char(int binary)
 {
 	int i;
-
 	i = 0;
-	while (argv[i])
-	{
-		if (ft_isdigit(argv[i]) == 1)
-			i++;
-		else
-			return (0);
-	}
-	return(1);
+	/*
+		convert binary to string after recieved it from the client  do i need a function to check if recieved or not
+		using bitwise operations
+	*/
 }
-   
 int main(int argc, char **argv)
 {
-	pid_t				pid;
-	int					i;
-	struct sigaction	sa;
+	pid_t pid;
 
-	sa.sa_handler = &handler;
-	sigaction(SIGINT, &sa, NULL);
-	if(argc == 3 && ft_isdigit_adv(argv[1]) == 1)
+	struct sigaction sa;
+	if(argc == 1)
 	{
-		pid = ft_atoi(argv[1]);
-		
-		
+		pid = getpid();
+		ft_printf("%d\n", pid);
+		while(1)
+		{
+			pause();
+		}
 	}
-
+	return(0);
 }
